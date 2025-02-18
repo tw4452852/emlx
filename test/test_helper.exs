0bf7e72e9f3051d60b37a9cfbcbdd9d80049b692
Cocoa <i@uwucocoa.moe>
2025-02-18 14:31:06 +0100

    chore(ci): test gpu backend on ci (#75)
    * chore(ci): test gpu backend on ci
    
    Signed-off-by: Cocoa <i@uwucocoa.moe>

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,3 +1,16 @@
-Application.put_env(:nx, :default_backend, EMLX.Backend)
+backend =
+  if String.downcase(System.get_env("EMLX_TEST_DEFAULT_GPU", "false")) in [
+       "1",
+       "true",
+       "yes",
+       "t",
+       "y"
+     ] do
+    {EMLX.Backend, device: :gpu}
+  else
+    EMLX.Backend
+  end
+
+Application.put_env(:nx, :default_backend, backend)
 
 ExUnit.start()
